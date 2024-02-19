3ecdea951b0e284adb1fa2522d0447ff714e9df0
lakshmi0398 <lakshmiaviligonda30@gmail.com>
2024-02-19 04:12:21 +0000

    Message

---
+++ src/main/java/com/example/nxttrendz1/NxtTrendzApplication.java
@@ -0,0 +1,11 @@
+package com.example.nxttrendz1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NxtTrendzApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NxtTrendzApplication.class, args);
+    }
+}
